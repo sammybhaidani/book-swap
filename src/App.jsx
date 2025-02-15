dd53a50646f094fedc54a4410933294dbe3760e2
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-15 18:27:51 +0000

    Display Books page component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Nav from "./components/Nav";
+import BooksPage from "./Pages/BooksPage";
 
 export default function App() {
     return (
+        <>
         <Nav/>
+        <BooksPage isClaimed={0}/>
+        </>
     )
 }
