aa4f80926f70b175288547187d1be108be99f5b3
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-15 20:26:24 +0000

    Add link to book details

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import H3 from "./atoms/H3";
 
 export default function SingleBook({img, title, author, genre, id}) {
     return (
+        <Link to={`/books/${id}`}>
         <div className="flex flex-col gap-3 border p-3 items-center">
             <img src={img} alt="" />
             <H3 text={title}/>
             <p>{author}</p>
             <p>{genre}</p>
         </div>
+        </Link>
     )
 }
