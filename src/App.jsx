307b10edb46c933c685c090e0ccf9622c63ce4c7
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-15 20:24:30 +0000

    Add routing for book details component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import BooksPage from "./Pages/BooksPage";
+import BookDetails from "./components/BookDetails";
 
 export default function App() {
     return (
-        <>
-        <Nav/>
-        <BooksPage isClaimed={0}/>
-        </>
+        <BrowserRouter>
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<BooksPage isClaimed={0}/>}/>
+                <Route path="/" element={<BooksPage isClaimed={1}/>}/>
+                <Route path="/books/:id" element={<BookDetails/>} />
+            </Routes>
+        </BrowserRouter>
+        
     )
 }
