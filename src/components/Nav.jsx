e2e529c4167969a9a2a0d5fc86409004c0b12137
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-15 20:27:56 +0000

    Add link to return to books page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import H1 from "./atoms/H1";
 
 export default function Nav() {
     return (
         <nav className="bg-cyan-950 text-white p-4 flex justify-center sm:justify-start">
+            <Link to={"/"}>
             <H1 text={"Book Swap"}/>
+            </Link>
         </nav>
     )
 }
