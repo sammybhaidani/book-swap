31bd65d32bcc1e65b07d3b66c4bbf244080e8c4d
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-15 18:26:37 +0000

    Create Book page to display books

---
+++ src/Pages/BooksPage.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import SingleBook from "../components/SingleBook";
+
+export default function BooksPage({isClaimed}) {
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {fetch(`https://book-swap-api.dev.io-academy.uk/api/books?claimed=${isClaimed}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.data)
+        setBooks(data.data);
+    })},[])
+
+
+    return (
+
+       <div className="grid grid-cols-1 sm:grid-cols-3 gap-3 p-3">
+        {books.map(book => (
+            <SingleBook 
+            key={book.id}
+            img={book.image}
+            title={book.title}
+            author={book.author}
+            genre={book.genre.name}
+            id={book.id}/>
+        ))}
+        </div>
+       
+    )
+}
