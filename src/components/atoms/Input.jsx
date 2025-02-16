9b4569748aa300f6f6d7f42fe2c52930731c8f31
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-16 21:47:01 +0000

    Create input component

---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,9 @@
+export default function Input({label, id, type = "text", placeholder, value, handleInput, error}) {
+    return (
+        <div className="flex flex-col">
+            {label && <label className="text-sm mb-1" htmlFor={id}>{label}</label>}
+            <input onChange={handleInput} className="border rounded p-1" type={type} id={id} placeholder={placeholder} value={value} />
+            {error && <p>{error}</p>}
+        </div>
+    )
+}
