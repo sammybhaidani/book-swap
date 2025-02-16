3a17bf3a33103c4ca080a20f5f6602cedfe08339
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-16 22:19:18 +0000

    Add nav links to both available and claimed books

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import H1 from "./atoms/H1";
+import NavLink from "./NavLink";
 
 export default function Nav() {
     return (
-        <nav className="bg-cyan-950 text-white p-4 flex justify-center sm:justify-start">
-            <Link to={"/"}>
-            <H1 text={"Book Swap"}/>
-            </Link>
+        <nav className="bg-cyan-950 text-white p-4 flex flex-col gap-3 items-center sm:flex-row  sm:justify-between">
+            <div>
+                <Link to={"/"}>
+                    <H1 text={"Book Swap"}/>
+                </Link>
+            </div>
+            <div className="flex gap-5 justify-center">
+                <NavLink text={"Available books"} address={"/"}/>
+                <NavLink text={"Claimed books"} address={"/claimed"}/>
+            </div>
         </nav>
     )
 }
