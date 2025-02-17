cd3260a9ef5cc5c1cb2d10d6d9876dd8c7ce3eb8
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-17 01:26:57 +0000

    Implement logic for returning a book

--- src/components/ReturnForm.jsx
+++ src/components/ReturnForm.jsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
 import H2 from "./atoms/H2";
 import Input from "./atoms/Input";
 
-export default function ReturnForm({name}) {
+export default function ReturnForm({id, name, handlePerson}) {
 
-    
+    const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
+
+     const data = {
+        email: email
+    }
+
+    const requestOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    function handleEmail(e) {
+        setEmail(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch(`https://book-swap-api.dev.io-academy.uk/api/books/return/${id}`, requestOptions)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            if (data.errors?.email && email) {
+                setEmailError(data.errors.email[0])
+            } else if(data.errors?.email && !email) {
+                setEmailError(data.errors.email[1]);
+            } else {
+                handlePerson(null)
+            }
+        })
+    }
+
     return (
-        <form className="flex flex-col gap-3 border rounded p-3 mx-auto w-full">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3 border rounded p-3 mx-auto w-full">
             <H2 text={"Want to return this book?"}/>
-            <Input label={`${name}'s email`} id={"email"} type={"email"} placeholder={"Email"}/>
+            <Input label={`${name}'s email`} id={"email"} placeholder={"Email"} handleInput={handleEmail} error={emailError}/>
             <Input type={"submit"} value={"Return"}/>
         </form>
     )
