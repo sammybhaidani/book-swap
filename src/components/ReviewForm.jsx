fea0c29020bd72534ab259a8c58d3d6b36e43b0a
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-17 17:52:21 +0000

    Fix layout and styling

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -3,17 +3,22 @@ import Input from "./atoms/Input";
 
 export default function ReviewForm() {
     return (
-        <div>
+        <div className="border rounded p-3 flex flex-col gap-3 mx-auto w-full">
             <H3 text={"Want to review this book?"}/>
             <Input label={"Name"} placeholder={"Name"}/>
-            <label for="ratings">Score:</label>
-                <select id="ratings" name="ratings">
+            <div>
+            <label className="text-sm" htmlFor="ratings">Score:</label>
+            <select className="border rounded ml-1" id="ratings" name="ratings">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="4">5</option>
-                </select>
+            </select>
+            </div>
+            <textarea className="border rounded p-1" rows={"5"} cols={"33"} placeholder="Review"/>
+            <Input type="submit" value={"Review"}/>
+
         </div>
     )
 }
