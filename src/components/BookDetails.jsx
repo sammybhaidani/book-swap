b97f0e0f18883319604623c26bcf3f068ea6c83f
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-17 16:18:17 +0000

    Add each book review to book details page

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -3,14 +3,17 @@ import { useParams } from "react-router-dom"
 import H1 from "./atoms/H1";
 import ClaimForm from "./ClaimForm";
 import ReturnForm from "./ReturnForm";
+import H2 from "./atoms/H2";
+import BookReview from "./BookReview";
 
 export default function BookDetails() {
 
     const {id} = useParams();
 
     const [bookData, setBookData] = useState([]);
-    const [genre, setBookGenre] = useState('')
-    const [person, setPerson] = useState('')
+    const [genre, setBookGenre] = useState('');
+    const [person, setPerson] = useState('');
+    const [bookReviews, setBookReviews] = useState([]);
 
     function handlePerson(name) {
         setPerson(name);
@@ -22,6 +25,8 @@ export default function BookDetails() {
         setBookData(data.data);
         setBookGenre(data.data.genre.name);
         setPerson(data.data.claimed_by_name);
+        setBookReviews(data.data.reviews);
+        console.log(data.data.reviews);
     })}, [])
 
     return (
@@ -39,6 +44,14 @@ export default function BookDetails() {
                 : <ClaimForm id={id} handlePerson={handlePerson}/>}
                 {person && <ReturnForm id={id} name={person} handlePerson={handlePerson}/>}
                 <p>{bookData.blurb}</p>
+                <H2 text={"Reviews"}/>
+                {bookReviews.map(review => (
+                    <BookReview 
+                    key={review.id}
+                    name={review.name}
+                    rating={review.rating}
+                    review={review.review}/>
+                ))}
             </div>
         </div>
     )
