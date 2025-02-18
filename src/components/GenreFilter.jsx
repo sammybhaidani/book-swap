b743609785b1629e5418f0101c9c5a4ff3f08fe2
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-18 18:46:17 +0000

    Display genres in drop down list and set the selected genre

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,9 +1,12 @@
-export default function GenreFilter({genres}) {
+export default function GenreFilter({genres, handleGenre}) {
     return(
         <div>
             <label htmlFor="genre">Filter by genre:</label>
-            <select id="genre">
+            <select id="genre" onChange={handleGenre}>
                 <option value="">None selected</option>
+                {genres.map(genre => (
+                    <option key={genre.id} value={genre.id}>{genre.name}</option>
+                ))}
             </select>
         </div>
     )
