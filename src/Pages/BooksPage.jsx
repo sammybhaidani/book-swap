80c5367ef4655ddb78a7e570a21d32a5ce7e914f
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-18 18:27:48 +0000

    Get genres from api and pass into genre filter component

--- src/Pages/BooksPage.jsx
+++ src/Pages/BooksPage.jsx
@@ -5,6 +5,7 @@ import GenreFilter from "../components/GenreFilter";
 export default function BooksPage({isClaimed}) {
 
     const [books, setBooks] = useState([])
+    const [genres, setGenres] = useState([]);
 
     useEffect(() => {fetch(`https://book-swap-api.dev.io-academy.uk/api/books?claimed=${isClaimed}`)
     .then(res => res.json())
@@ -12,10 +13,17 @@ export default function BooksPage({isClaimed}) {
         setBooks(data.data);
     })},[isClaimed])
 
+    useEffect(() => {fetch('https://book-swap-api.dev.io-academy.uk/api/genres')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    })
+
     return (
         <div className="p-3">
-            
-        <GenreFilter/>
+
+        <GenreFilter genres={genres}/>
 
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
             {books.map(book => (
