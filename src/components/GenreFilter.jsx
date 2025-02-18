8dbfb766aa54c631d9efc88f9f49d4cee4fbaceb
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-18 19:02:07 +0000

    Style genre filter

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,8 +1,8 @@
 export default function GenreFilter({genres, handleGenre}) {
     return(
-        <div>
+        <div className="flex flex-col md:flex-row gap-3 items-center sm:items-start">
             <label htmlFor="genre">Filter by genre:</label>
-            <select id="genre" onChange={handleGenre}>
+            <select className="w-3xs border rounded p-1" id="genre" onChange={handleGenre}>
                 <option value="">None selected</option>
                 {genres.map(genre => (
                     <option key={genre.id} value={genre.id}>{genre.name}</option>
