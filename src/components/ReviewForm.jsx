bc1187df5c43511df5438bed8f7b7851cdbb12a8
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-18 17:50:29 +0000

    Clear form after submission and update review count state

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,19 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import H3 from "./atoms/H3";
 import Input from "./atoms/Input";
 import FormError from "./atoms/FormError";
 
-export default function ReviewForm({id, handleReviewCount, reviewCount, handleReviewSubmit}) {
+export default function ReviewForm({id, handleReviewCount}) {
 
     const [name, setName] = useState("");
-    const [rating, setRating] = useState(null);
+    const [rating, setRating] = useState("-");
     const [review, setReview] = useState("");
     const [nameError, setNameError] = useState("");
     const [reviewError, setReviewError] = useState("");
     const [ratingError, setRatingError] = useState("");
-    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-
-    useEffect(clearForm, [isFormSubmitted]);
 
     const data = {
         name: name,
@@ -44,10 +41,9 @@ export default function ReviewForm({id, handleReviewCount, reviewCount, handleRe
     }
 
     function clearForm() {
-        setIsFormSubmitted(true);
         setName("");
         setReview("");
-        setRating(1);
+        setRating("-");
     }
 
     function handleSubmit(e) {
@@ -55,29 +51,25 @@ export default function ReviewForm({id, handleReviewCount, reviewCount, handleRe
         fetch('https://book-swap-api.dev.io-academy.uk/api/reviews', requestOptions)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
 
             if(data?.errors) {
                 setNameError(data.errors.name);
                 setReviewError(data.errors.review);
-                rating != '-' && setRatingError(data.errors.rating);
+                setRatingError(data.errors.rating);
+            } else {
+                handleReviewCount();
+                clearForm();
             }
-            
-            clearForm();
-           
-            // handleReviewCount(reviewCount + 1)
-            // handleReviewSubmit(true);
         })
-
     }
 
      return (
         <form onSubmit={handleSubmit} className="border rounded p-3 flex flex-col gap-3 mx-auto w-full">
             <H3 text={"Want to review this book?"}/>
-            <Input label={"Name"} placeholder={"Name"} handleInput={handleName} error={nameError}/>
+            <Input label={"Name"} placeholder={"Name"} handleInput={handleName} error={nameError} value={name}/>
             <div>
             <label className="text-sm" htmlFor="ratings">Score:</label>
-            <select onChange={handleRating} className="border rounded ml-1" id="ratings" name="ratings">
+            <select onChange={handleRating} className="border rounded ml-1" id="ratings" name="ratings" value={rating}>
                 <option value="">-</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -87,7 +79,7 @@ export default function ReviewForm({id, handleReviewCount, reviewCount, handleRe
             </select>
             {ratingError && <FormError text={ratingError}/>}
             </div>
-            <textarea onChange={handleReview} className="border rounded p-1" rows={"5"} cols={"33"} placeholder="Review"/>
+            <textarea onChange={handleReview} className="border rounded p-1" rows={"5"} cols={"33"} placeholder="Review" value={review}/>
             {reviewError && <FormError text={reviewError}/>}
             <Input type="submit" value={"Review"}/>
         </form>
